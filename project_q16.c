18da9868818ad11eb98570fe88803b7f4c262f4d
SouvickMondal <souvickmondalp7@gmail.com>
2018-03-25 09:57:08 +0530

    Optimal Version Released with both questions implemented and bugs fixed

--- project_q16.c
+++ project_q16.c
@@ -1,48 +1,25 @@
-//Scheduler
-
-
-//Ques. 16. Design a scheduler that can schedule the processes arriving system at periodical intervals. 
-//Every process is assigned with a fixed time slice t milliseconds. 
-//If it is not able to complete its execution within the assigned time quantum, then automated timer generates an interrupt. 
-//The scheduler will select the next process in the queue and dispatcher dispatches the process to processor for execution. 
-//Compute the total time for which processes were in the queue waiting for the processor. 
-//Take the input for CPU burst, arrival time and time quantum from the user. 
- 
-//Author: SOuvick Mondal
-
-
 #include<stdio.h>
-#include<conio.h>
-#include<stdbool.h>
-
-
+#include<conio.h> 
 
 struct ProcessControlBlock
 {
 	char process_name;
-	int process_id;
 	int arrival_time;
 	int burst_time;
 	int burst_time1;
 	int completion_time;
 	int turn_around_time;
-	int process_completion_status;
-	int process_readyqueue_status;
 	int process_waiting_time;
 	
 }p[20];
 
-struct ProcessControlBlock *ready_queue[100];
+struct ProcessControlBlock *ready_queue[20];
 int front = -1;
 int rear = -1;
-int waiting_time[20];
-int wt = 0;	
-int avg_waiting_time;
-int total_waiting_time = 0;
 int flag=0;
 
 
-					char qinsert(struct ProcessControlBlock * item)
+					char qinsert(struct ProcessControlBlock  *item)
 						{
 							if(front<0)
 							{
@@ -54,7 +31,7 @@ int flag=0;
 								rear = rear + 1;
 							}
 							ready_queue[rear] = item;
-							return item->process_name;
+							return ready_queue[rear]->process_name;
 						}
 						
 						char qdelete()
@@ -89,50 +66,20 @@ int main()
 	scanf("%d",&time_quantum);
 	printf("\n<<<<<   Thank you for entering the Time Quantum   >>>>>>\n");
 	
-	//Process Completion Status Check Function
-	
-	/* int check()
-   {
-   	int i1 = 0;
-   	for(i1 = 0;i1<number_of_process;i1 ++)
-   	{
-   		if(p[i1].process_completion_status!=2)
-   		{
-   			break;
-		   }
-		   
-	   }
-	 if(i1 == number_of_process)
-	 {
-	 	return 2;
-	   } 
-	   else
-	   {
-	   	return 3;
-		} 
-   	
-	}		
-	*/
-	
-	
 	
 	for(a = 0; a < number_of_process; a++)
 	{
 		int temp;
 		p[a].process_name = 'A'+a;
-		p[a].process_id = 101 + a;
-		p[a].process_completion_status = -1;
-		p[a].process_readyqueue_status = -1;
 		
 		printf("\nEnter Process Arraival Time for the process %c : ",p[a].process_name);
 		scanf("%d",&p[a].arrival_time);
 		printf("\nEnter Process Burst Time for the process having Process %c : ",p[a].process_name);
 		scanf("%d",&p[a].burst_time);
-		temp = p[a].burst_time;
-		 p[a].burst_time1 = temp;
+		 p[a].burst_time1 = p[a].burst_time;
 		
 	}
-	//To Find Starting Time
+	//To Find Starting Time	
 	
 	int loop;
 	int min=p[0].arrival_time;
@@ -144,38 +91,21 @@ int main()
 		}
 	}
 	time = min;
+	
+	
 	while(flag<number_of_process)
 	{
-	/*	int i = 0;
-		while(i<number_of_process)
-		{
-			if(p[i].process_completion_status<0)
-			{
-				break;
-			}
-			else
-			{
-				i = i + 1;
-			}
-		}
-		if(i == number_of_process)
-		{
-			return 0;
-		}
-	*/	
-	//else
-	//	{
+	
 			for(j = 0; j < time_quantum; j++)
 			{
 				
 				//Arrival Department Start
 				for(k = 0; k < number_of_process; k++)
-				{
+				{	
 					if(p[k].arrival_time == time)
 					{
-						p[k].process_readyqueue_status = 0;
 						qinsert(&p[k]);
-						printf("\n Process %c is inserted in the Ready Queue.\n",p[k].process_name);
+						//printf("\n Process %c is inserted in the Ready Queue.\n",p[k].process_name);
 						
 					}
 				}
@@ -183,54 +113,52 @@ int main()
 			ready_queue[front]->burst_time = ready_queue[front]->burst_time - 1;
 			time = time + 1;
 			 
-			//Selecting Process from the Ready Queue
+			 
 			if(ready_queue[front]->burst_time==0)
 			{
-				printf("\nProcess %c is Finished.\n",ready_queue[front]->process_name);
-				printf("\nProcess %c is Finishing Time: %d.\n",ready_queue[front]->process_name,time);
+				printf("\nProcess %c is Finished at Time: %d.\n",ready_queue[front]->process_name,time);
 			
-				ready_queue[front]->turn_around_time = time - ready_queue[front]->arrival_time;
-				
-				
-				ready_queue[front]->process_completion_status = 0;
+				ready_queue[front]->completion_time = time;
 				flag = flag + 1;
-			qdelete();	
+				qdelete();	
 			}
 			
 
-				//Arrival Finish
 				
-				//time = time + 1;
-				//if(ready_queue[front].burst_time)
-				
-			}
-			
-			if(ready_queue[front]->burst_time > 0)
-		{
-			struct ProcessControlBlock *last = &ready_queue[front];
-			
-			qdelete();
-			qinsert(&last);
-			
 		}
+			
+			
+			
+			if(ready_queue[front]->burst_time>0)
+			{
+				struct ProcessControlBlock *last = ready_queue[front];
+			
+				qdelete();
+				qinsert(last);
+				printf("\nProcess %c is interrupted at time = %d. ",last->process_name,time);
+			
+			
+			}
 
 		
-							}
-	//}
+	}
 	
-int m;
-for(m=0;m<number_of_process;m++)
+	
+/*for(m=0;m<number_of_process;m++)
 {
 	printf("\n");
 	printf("%d",m);
 	printf("\n");
 	printf("%d",p[m].burst_time1);
-/*	p[m].process_waiting_time = p[m].turn_around_time - p[m].burst_time1;
-	total_waiting_time = p[m].process_waiting_time + total_waiting_time;*/
-}	
-	/*avg_waiting_time = total_waiting_time/number_of_process;
+	p[m].process_waiting_time = p[m].turn_around_time - p[m].burst_time1;
+	total_waiting_time = p[m].process_waiting_time + total_waiting_time;
+}
+	avg_waiting_time = total_waiting_time/number_of_process;
 	printf("\nTotal Waiting Time: %d\n",total_waiting_time);
     printf("\nAvarage Waiting Time: %d\n",avg_waiting_time);
+
+
 	*/
+	return 0;
 }
 
